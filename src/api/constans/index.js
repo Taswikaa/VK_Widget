03221178e3b11d328b75517cb5e9c911378d6097
Alekseev Sergey <alekseev.s.v1@yandex.ru>
2023-11-23 19:11:21 +0300

    добавлен класс для взаимодействия с api

---
+++ src/api/constans/index.js
@@ -0,0 +1,7 @@
+export const BASE_URL = 'https://api.vk.com/method/wall.get?';
+export const ACESS_TOKEN = '9ba525539ba525539ba525539298b3404b99ba59ba52553fefb194235dae1c4d5da73e5';
+export const API_VERSION = '5.199';
+// Количество получаемых постов за запрос
+export const COUNT = '10';
+
+// fetchJsonp("owner_id=-94253821&count=5&offset=0")
--- src/components/Widget/Widget.jsx
+++ src/components/Widget/Widget.jsx
@@ -1,8 +1,23 @@
+import { useEffect } from 'react'
 import './Widget.css'
+import { api } from '../../api/api'
 
 const Widget = () => {
+
+  useEffect(() => {
+    api.getGroupId(186103254);
+  }, [])
+
+  function handleScroll() {
+    console.log(123);
+  }
+
+  function getData() {
+    api.getPosts(0);
+  }
+
   return (
-    <div className='widget widget_size_small'>
+    <div className='widget widget_size_small' onScroll={handleScroll} onClick={getData}>
       <div className='post'>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum repellat nihil corporis dicta commodi, soluta blanditiis rerum, consequuntur inventore delectus ipsum rem eligendi, velit neque ipsam earum nostrum architecto voluptatibus?</p>
         <img src="https://images.unsplash.com/photo-1700629691463-3d8a9294a80c?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
